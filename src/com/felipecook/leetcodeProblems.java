f46de84a77bb5f67cf24a4632be769adf91b41e7
Felipe Cook <felipepcook@gmail.com>
2020-04-11 08:48:38 -0600

    Initial commit + singleNumber method

---
+++ src/com/felipecook/leetcodeProblems.java
@@ -0,0 +1,20 @@
+package com.felipecook;
+
+public class leetcodeProblems {
+
+  /*Given a non-empty array of integers, every element appears twice except for one. Find that single one.*/
+  public int singleNumber(int[] nums) {
+    
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    for (int i = 0; i < nums.length; i++) {
+
+      for (int j = i + 1; j < nums.length; j++) {
+
+      }
+
+    }
+  }
+
+}
