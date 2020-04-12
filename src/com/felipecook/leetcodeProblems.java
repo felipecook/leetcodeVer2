c3b52f8986ebd6883769e3f0cf327b8931f74677
Felipe Cook <felipepcook@gmail.com>
2020-04-12 08:51:14 -0600

    isHappy Number method + initial commit

--- src/com/felipecook/leetcodeProblems.java
+++ src/com/felipecook/leetcodeProblems.java
@@ -37,4 +37,39 @@ public class leetcodeProblems {
     return 0;
   }
 
+  /*A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.*/
+  public static boolean isHappy(int n) {
+
+    if (n == 1) {
+      return true;
+    }
+    if (n < 1) {
+      return false;
+    }
+
+    boolean flag = true;
+    int sum = 0;
+    while (flag) {
+
+      int lastDigit = n % 10;
+      sum += lastDigit * lastDigit;
+
+      n /= 10;
+
+      if (n < 1) {
+        flag = false;
+      }
+    }
+
+    isHappy(sum);
+
+
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(isHappy(19));
+
+  }
+
 }
