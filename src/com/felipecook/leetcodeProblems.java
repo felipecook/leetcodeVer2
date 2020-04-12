09f72e75e21e1333c7d6a4668b96cab48481cad5
Felipe Cook <felipepcook@gmail.com>
2020-04-12 09:10:21 -0600

    isHappy Number method + solution (time limit exceeded)

--- src/com/felipecook/leetcodeProblems.java
+++ src/com/felipecook/leetcodeProblems.java
@@ -40,28 +40,26 @@ public class leetcodeProblems {
   /*A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.*/
   public static boolean isHappy(int n) {
 
-    if (n == 1) {
-      return true;
-    }
-    if (n < 1) {
-      return false;
-    }
+    while(n > 1) {
 
-    boolean flag = true;
-    int sum = 0;
-    while (flag) {
+      boolean flag = true;
+      int sum = 0;
+      int reflectionOfN = n;
+      while(flag) {
+        int lastDigit = reflectionOfN % 10;
+        sum += lastDigit * lastDigit;
 
-      int lastDigit = n % 10;
-      sum += lastDigit * lastDigit;
+        reflectionOfN /= 10;
 
-      n /= 10;
-
-      if (n < 1) {
-        flag = false;
+        if(reflectionOfN < 1) {
+          flag = false;
+        }
       }
+      n = sum;
+
     }
 
-    isHappy(sum);
+    return n == 1;
 
 
   }
