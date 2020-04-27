e8fdb2a66b95d93527609d0c88a235631e95c47d
Felipe Cook <felipepcook@gmail.com>
2020-04-27 16:19:17 -0600

    + fixing errors

--- src/com/felipecook/leetcodeProblems.java
+++ src/com/felipecook/leetcodeProblems.java
@@ -40,7 +40,7 @@ public class leetcodeProblems {
   /*A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.*/
   public static boolean isHappy(int n) {
 
-    if(n)
+   // if(n)
 
     while(n > 1) {
 
@@ -101,6 +101,7 @@ public class leetcodeProblems {
 
     }
 
+    return 0;
   }
 
 
