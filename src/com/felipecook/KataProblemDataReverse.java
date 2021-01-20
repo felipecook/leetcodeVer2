d8f922cbb009473ebbf70a44ba14222a7339f4c6
Felipe Cook <felipepcook@gmail.com>
2021-01-20 16:09:56 -0700

    + initial solution to KataProblemDataReverse

--- src/com/felipecook/KataProblemDataReverse.java
+++ src/com/felipecook/KataProblemDataReverse.java
@@ -4,8 +4,21 @@ public class KataProblemDataReverse {
 
   public static int[] DataReverse(int[] data) {
 
-    
+    int[] returnArray = new int[data.length];
 
+    int countingBackwards = data.length - 1;
+
+
+
+    for (int i = 0; i < data.length; i++) {
+
+      returnArray[i] = data[countingBackwards];
+      countingBackwards--;
+
+
+    }
+
+    return returnArray;
 
   }
 
