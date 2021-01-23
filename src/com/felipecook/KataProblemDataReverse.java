97c7e52049f3e5725ebd6a4ead94c9405c02383e
Felipe Cook <felipepcook@gmail.com>
2021-01-22 20:49:36 -0700

    + new solution to KataProblemDataReverse

--- src/com/felipecook/KataProblemDataReverse.java
+++ src/com/felipecook/KataProblemDataReverse.java
@@ -4,18 +4,39 @@ public class KataProblemDataReverse {
 
   public static int[] DataReverse(int[] data) {
 
+    if (data.length == 8) {
+      return data;
+    }
+
     int[] returnArray = new int[data.length];
 
-    int countingBackwards = data.length - 1;
+    int counter = 0;
 
+    int reverseCounter = data.length - 1;
 
-
+    int[] byteOfData = new int[8];
+    
     for (int i = 0; i < data.length; i++) {
 
-      returnArray[i] = data[countingBackwards];
-      countingBackwards--;
+      byteOfData[counter] = data[i];
 
+      counter++;
 
+      // if the counter has reached 8, that means that we have a full byte of data
+      if (counter == 8) {
+        for (int j = reverseCounter; j > reverseCounter - 8; j--) {
+
+          returnArray[j] = byteOfData[counter - 1];
+
+          counter--;
+        }
+
+        
+      }
+
+      
+
+      
     }
 
     return returnArray;
---
+++ test/com/felipecook/KataProblemDataReverseTest.java
@@ -0,0 +1,14 @@
+package com.felipecook;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KataProblemDataReverseTest {
+
+  int[] testData = {1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0, 0,0,0,0,1,1,1,1, 1,0,1,0,1,0,1,0};
+
+  @Test
+  void dataReverse() {
+  }
+}
