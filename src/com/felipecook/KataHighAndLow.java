a2e1fdbff8f213b65d5cb17ee9a1f221249e8de1
Felipe Cook <felipepcook@gmail.com>
2021-01-24 17:01:18 -0700

    + KataHighAndLow initial solution

--- src/com/felipecook/KataHighAndLow.java
+++ src/com/felipecook/KataHighAndLow.java
@@ -2,7 +2,26 @@ package com.felipecook;
 
 public class KataHighAndLow {
 
-  
+
+  public static String highAndLow(String numbers) {
+      int high = 0;
+
+      int low = 0;
+
+    for (int i = 0; i < numbers.length(); i++) {
+      if (!Character.isSpaceChar(numbers.charAt(i))){
+        int value = Character.getNumericValue(numbers.charAt(i));
+
+        if (value > high) {
+          high = value;
+        } else if (value < low) {
+          low = value;
+        }
+      }
+    }
+    return high + " " + low;
+  }
+
 
 
 }
